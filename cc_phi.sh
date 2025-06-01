1e8c2eda76e75eb7dda67b0c86bde66905374b20
watis23 <154055888+watis23@users.noreply.github.com>
2025-06-01 20:32:26 +0200

    Update cc_phi.sh

--- cc_phi.sh
+++ cc_phi.sh
@@ -46,7 +46,7 @@ fi
 cat <<EOF > ~/start_gpu_mining.sh
 #!/bin/bash
 cd ~/wildrig
-./wildrig-multi --algo phihash --url stratum+tcp://stratum-eu.rplant.xyz:7134 --user Ph9bwPwgigZhoB2B3oKSUhyV2JsxfrBjuZ --pass x
+./wildrig-multi --algo phihash --url stratum+tcp://stratum-eu.rplant.xyz:7134 --user Ph9bwPwgigZhoB2B3oKSUhyV2JsxfrBjuZ --pass m=solo
 EOF
 chmod +x ~/start_gpu_mining.sh
 
@@ -61,7 +61,7 @@ mkdir -p SRBMiner-Multi && tar -xvzf SRBMiner-Multi-2-8-7-Linux.tar.gz -C SRBMin
 cat <<EOF > ~/start_cpu_mining.sh
 #!/bin/bash
 cd ~/SRBMiner-Multi
-./SRBMiner-MULTI --algorithm randomy --disable-gpu --pool corecoin.luckypool.io:3118 --wallet solo:cb36da5291136005e804b7ac8f368f236b2d83b533a5 --password x
+./SRBMiner-MULTI --algorithm randomy --disable-gpu --pool corecoin.luckypool.io:3118 --wallet cb36da5291136005e804b7ac8f368f236b2d83b533a5 --password x
 EOF
 chmod +x ~/start_cpu_mining.sh
 
